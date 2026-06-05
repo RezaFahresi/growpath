fc4f44e5266d7174f9b44ecba2002bf7ffc5fdfc
Reza Fahresi <Fahrezireza26@gmail.com>
2026-06-05 17:37:53 +0700

    Fix: Sederhanakan baseURL axios untuk Vercel Monorepo

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Kita tambahkan + '/api' di belakang URL Vercel
-  // Dan kita gunakan '/api' untuk mode lokal agar ditangkap oleh proxy Vite
-  baseURL: import.meta.env.PROD 
-    ? import.meta.env.VITE_API_URL + '/api' 
-    : '/api', 
+  // Cukup gunakan '/api'. 
+  // Di lokal akan diurus oleh Vite Proxy, di Production akan diurus oleh Vercel Rewrites.
+  baseURL: '/api', 
   withCredentials: true, 
 });
 
