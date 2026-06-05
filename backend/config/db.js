b2a8071d18218aeeb06d882972f9f2887320a40f
Reza Fahresi <Fahrezireza26@gmail.com>
2026-06-05 20:01:21 +0700

    use supabase

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,16 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  // HANYA gunakan connectionString ini, jangan ada host, user, atau password terpisah
+  connectionString: process.env.DATABASE_URL, 
   ssl: {
     rejectUnauthorized: false
   }
+});
+
+pool.connect((err) => {
+  if (err) console.error("🚨 Database Connection Error:", err.stack);
+  else console.log("✅ Supabase Connected!");
 });
 
 module.exports = pool;
