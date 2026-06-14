750feafd2e2435f3282cd7730db86f038ec4d532
Reza Fahresi <Fahrezireza26@gmail.com>
2026-06-14 15:49:53 +0700

    Fix Bug Part 8

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, Link, useLocation } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { useAppContext } from '../context/AppContext';
 import API from '../api/axios'; 
 import Swal from 'sweetalert2';
